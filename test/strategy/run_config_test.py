db7d96c91597b598ca35387fa0f9801c98edaafb
InsonusK <insonus.k@gmail.com>
2023-10-23 15:20:29 +0000

    FI-82 #done add code

--- test/strategy/run_config_test.py
+++ test/strategy/run_config_test.py
@@ -1,6 +1,7 @@
+from dataclasses import FrozenInstanceError
 import unittest
 import logging
-from src.strategy.run_config import RunConfig
+from src.strategy.run_config import RunConfig, date
 
 
 class RunConfig_TestCase(unittest.TestCase):
@@ -11,21 +12,58 @@ class RunConfig_TestCase(unittest.TestCase):
 
   def test_WHEN_construct_THEN_construct_correctly(self):
     # Array
-
+    rc = RunConfig(["A", "B"], date(2020, 1, 1),
+                   date(2021, 1, 1), {"p1": 1, "p2": 2})
     # Act
 
     # Assert
+    self.assertEqual(2, len(rc.stock_list))
+    self.assertIn("A", rc.stock_list)
+    self.assertIn("B", rc.stock_list)
+    self.assertEqual(date(2020, 1, 1), rc.from_date)
+    self.assertEqual(date(2021, 1, 1), rc.until_date)
+    self.assertEqual(2, len(rc.parameters.keys()))
+    self.assertEqual(1, rc.parameters["p1"])
+    self.assertEqual(2, rc.parameters["p2"])
 
   def test_WHEN_construct_THEN_it_is_imutable(self):
     # Array
+    rc = RunConfig(["A", "B"], date(2020, 1, 1),
+                   date(2021, 1, 1), {"p1": 1, "p2": 2})
 
     # Act
+    with self.assertRaises(AttributeError):
+      rc.stock_list.append("C")
+    with self.assertRaises(AttributeError):
+      rc.stock_list = ['C']
+    with self.assertRaises(AttributeError):
+      rc.from_date = date(2020, 1, 2)
+    with self.assertRaises(AttributeError):
+      rc.until_date = date(2021, 1, 2)
+    with self.assertRaises(TypeError):
+      rc.parameters["p1"] = 2
+    with self.assertRaises(AttributeError):
+      rc.parameters = {"p3": 3}
 
     # Assert
+    self.assertEqual(2, len(rc.stock_list))
+    self.assertIn("A", rc.stock_list)
+    self.assertIn("B", rc.stock_list)
+    self.assertEqual(date(2020, 1, 1), rc.from_date)
+    self.assertEqual(date(2021, 1, 1), rc.until_date)
+    self.assertEqual(2, len(rc.parameters.keys()))
+    self.assertEqual(1, rc.parameters["p1"])
+    self.assertEqual(2, rc.parameters["p2"])
+
 
   def test_WHEN_hash_or_equal_compare_THEN_compare_correctly(self):
     # Array
-
+    rc1 = RunConfig(["A", "B"], date(2020, 1, 1),
+                    date(2021, 1, 1), {"p1": 1, "p2": 2})
+    rc2 = RunConfig(["A", "B"], date(2020, 1, 1),
+                    date(2021, 1, 1), {"p1": 1, "p2": 2})
     # Act
 
     # Assert
+    self.assertEqual(hash(rc1), hash(rc2))
+    self.assertEqual(rc1, rc2)
