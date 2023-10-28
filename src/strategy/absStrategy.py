4eaee510b516401695f75d592123e27f8c40d5c2
InsonusK <insonus.k@gmail.com>
2023-10-28 19:17:06 +0000

    FI-86 #done

--- src/strategy/absStrategy.py
+++ src/strategy/absStrategy.py
@@ -1,35 +1,34 @@
 from __future__ import annotations
 from .deal import Deal
 from abc import ABC
-from datetime import date, datetime
+from datetime import sdatetime
 from typing import Dict, List
 from .run_config import MarketConfig
 
+
 class absStrategy(ABC):
-  # TODO [FI-85]: Описать abstract Strategy
-  """Abstraction of strategy
-  """
-
-  @property
-  def abs_capital_log(self) -> Dict[datetime, float]:
-    """abstract capital log. Initial capital start from 1 other capital based on this value
-
-    Returns:
-        Dict[datetime, float]: Log of changing base capital
+    """Abstraction of strategy
     """
-    ...
 
-  @property
-  def deal_list(self) -> List[Deal]:
-    ...
+    @property
+    def abs_capital_log(self) -> Dict[datetime, float]:
+        """abstract capital log. Initial capital start from 1 other capital based on this value
 
-  def run(self, market_cfg: MarketConfig) -> absStrategy:
-    """run strategy for maket list on period from_date untill_date
+        Returns:
+            Dict[datetime, float]: Log of changing base capital
+        """
+        ...
 
-    Args:
-        stock_list (List[str]): _description_
-        from_date (date): _description_
-        untill_date (date): _description_
-    """
-    ...
+    @property
+    def deal_list(self) -> List[Deal]:
+        ...
 
+    def run(self, market_cfg: MarketConfig) -> absStrategy:
+        """run strategy for maket list on period from_date untill_date
+
+        Args:
+            stock_list (List[str]): _description_
+            from_date (date): _description_
+            untill_date (date): _description_
+        """
+        ...
--- src/strategy/absStrategyFactory.py
+++ src/strategy/absStrategyFactory.py
@@ -1,14 +1,11 @@
-from src.strategy.absStrategy import absStrategy
-
-
+from .absStrategy import absStrategy
+from .run_config import StrategyConfig
 from abc import ABC
-from typing import Dict
 
 
 class absStrategyFactory(ABC):
-  # TODO: [FI-86] Описать abstract Strategy.Factory
   """Factory for builing new strategy instance in each test
   """
 
-  def build(self, parameters: Dict) -> absStrategy:
+  def build(self, parameters: StrategyConfig) -> absStrategy:
     ...
